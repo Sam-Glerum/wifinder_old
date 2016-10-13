eefc473488165bcd4fb44848a8f2c09c6d0e9415
sam-glerum <samuelglerum@hotmail.com>
2016-10-13 11:40:20 +0200

    integrated check for checking if interface exists in system

--- wifinder.py
+++ wifinder.py
@@ -4,5 +4,12 @@ import os
 # set the wireless interface used for scanning
 def get_network_interface():
     network_interface = raw_input("Name of scanning interface: ")
+    interfaces = os.listdir('/sys/class/net')
     os.system('clear')
-    print("Using interface: {0}").format(network_interface)
+    if network_interface in interfaces:
+        print("Using interface: {0}").format(network_interface)
+    else:
+        print "Wrong interface, try again"
+        get_network_interface()
+
+get_network_interface()
