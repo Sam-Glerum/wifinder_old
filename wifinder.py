47c5856952b44cc73bf06c3ac0b56ce0ecc80d97
Sam-Glerum <samuelglerum@hotmail.com>
2016-12-22 12:24:38 +0100

    added function for listing network interfaces. Changed the format of get_network_interface function

--- wifinder.py
+++ wifinder.py
@@ -1,20 +1,29 @@
-# Wifinder
+""" Wifinder """
 import os
 
-# set the wireless interface used for scanning
+
+interfaces = os.listdir('/sys/class/net')
+
+
+def list_network_interfaces():
+    for interface in interfaces:
+        print("Interface: " + interface)
+        print("=" * 20)
 
 
 def get_network_interface():
-    network_interface = raw_input("Name of scanning interface: ").lower()
-    interfaces = os.listdir('/sys/class/net')
+    # set the wireless interface used for scanning
+    network_interface = input("Name of scanning interface: ").lower()
     os.system('clear')
-
     # check if interface exists
     if network_interface in interfaces:
-        print("Using interface: {0}").format(network_interface)
+        print("Using interface: {}".format(network_interface))
     else:
-        print "Wrong interface, try again"
-        print "=" * len("Wrong interface, try again")
+        print("Wrong interface, try again")
+        print("=" * len("Wrong interface, try again"))
+        list_network_interfaces()
         get_network_interface()
 
+
+list_network_interfaces()
 get_network_interface()
