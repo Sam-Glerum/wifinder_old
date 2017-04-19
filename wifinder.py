afc9845c3ead60bd6fac492ac69d50811b4982ea
sam-glerum <samuelglerum@hotmail.com>
2017-04-19 12:06:12 +0200

    added ssid functionality

--- wifinder.py
+++ wifinder.py
@@ -1,6 +1,7 @@
 """ Wifinder """
 import os
 import scapy
+import subprocess
 
 
 interfaces = os.listdir('/sys/class/net')
@@ -14,11 +15,13 @@ def list_network_interfaces():
 
 def get_network_interface():
     # set the wireless interface used for scanning
-    network_interface = input("Name of scanning interface: ").lower()
+    network_interface = raw_input("Name of scanning interface: ").lower()
     os.system('clear')
+
     # check if interface exists
     if network_interface in interfaces:
         print("Using interface: {}".format(network_interface))
+        return network_interface
     else:
         print("Wrong interface, try again")
         print("=" * len("Wrong interface, try again"))
@@ -27,8 +30,11 @@ def get_network_interface():
 
 
 def sniff_ssid():
+    network_interface = get_network_interface()
+    subprocess.call(['airmon-ng', {}]).format(network_interface)
     pass
 
 
 list_network_interfaces()
 get_network_interface()
+sniff_ssid()
